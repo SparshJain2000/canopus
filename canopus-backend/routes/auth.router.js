e88f61c6195ac63ede36cdbb034bc541c084c6f8
SparshJain2000 <jainsparsh0801@gmail.com>
2020-08-07 22:34:53 +0530

    User : facebook auth (get user email)

--- canopus-backend/routes/auth.router.js
+++ canopus-backend/routes/auth.router.js
@@ -9,14 +9,24 @@ router.get(
 );
 router.get(
     "/google/callback",
-
-    passport.authenticate("google"),
-    (req, res) => {
-        // res.json({ user: req.user });
-
-        res.redirect("http://localhost:3000/search-jobs");
+    (req, res, next) => {
+        passport.authenticate("google", (err, user, info) => {
+            console.log(info);
+            if (err) {
+                console.log(err);
+                res.redirect(`http://localhost:3000?err=${err.name}`);
+            }
+            if (!user) {
+                res.redirect(`http://localhost:3000?err=${err.name}`);
+            }
+            req.logIn(user, function (err) {
+                if (err) {
+                    res.redirect(`http://localhost:3000?err=${err.name}`);
+                }
+                res.redirect(`http://localhost:3000/`);
+            });
+        })(req, res, next);
     },
-
     // function (req, res) {
     //     console.log(req.user);
     //     var token = req.user.token ? req.user.token : "";
@@ -26,18 +36,30 @@ router.get(
     // },
 );
 
-router.get("/facebook", passport.authenticate("facebook"));
-
 router.get(
-    "/facebook/callback",
+    "/facebook",
     passport.authenticate("facebook", {
         authType: "reauthenticate",
-        scope: ["user_friends", "manage_pages", "email"],
+        scope: ["email"],
     }),
-    (req, res) => {
-        console.log(req.user);
-        res.redirect("http://localhost:3000/profile");
-    },
 );
+
+router.get("/facebook/callback", (req, res, next) => {
+    passport.authenticate("facebook", (err, user, info) => {
+        console.log(info);
+        if (err)
+            // return res.status(400).json({ err: err });
+            res.redirect(`http://localhost:3000?err=${err.name}`);
+
+        if (!user) res.redirect(`http://localhost:3000?err=${err.name}`);
+
+        req.logIn(user, function (err) {
+            if (err) {
+                res.redirect(`http://localhost:3000?err=${err.name}`);
+            }
+            res.redirect(`http://localhost:3000/`);
+        });
+    })(req, res, next);
+});
 
 module.exports = router;
