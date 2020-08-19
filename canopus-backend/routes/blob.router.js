969680d6894e4ef5c2588a0c124aae55fec05eb8
SparshJain2000 <jainsparsh0801@gmail.com>
2020-08-19 23:30:23 +0530

    Image upload : sas token

--- canopus-backend/routes/blob.router.js
+++ canopus-backend/routes/blob.router.js
@@ -9,8 +9,47 @@ AZURE_STORAGE_CONNECTION_STRING =
     process.env.AZURE_STORAGE_CONNECTION_STRING || "";
 
 var blobService = azure.createBlobService();
+function generateSasToken(container, blobName, permissions) {
+    var connString = process.env.AzureWebJobsStorage;
 
-router.post("/", (req, res) => {
+    // Create a SAS token that expires in an hour
+    // Set start time to five minutes ago to avoid clock skew.
+    var startDate = new Date();
+    startDate.setMinutes(startDate.getMinutes() - 5);
+    var expiryDate = new Date(startDate);
+    expiryDate.setMinutes(startDate.getMinutes() + 60);
+
+    permissions =
+        permissions || azure.BlobUtilities.SharedAccessPermissions.WRITE;
+
+    var sharedAccessPolicy = {
+        AccessPolicy: {
+            Permissions: permissions,
+            Start: startDate,
+            Expiry: expiryDate,
+        },
+    };
+
+    var sasToken = blobService.generateSharedAccessSignature(
+        container,
+        blobName,
+        sharedAccessPolicy,
+    );
+
+    return {
+        token: sasToken,
+        uri: blobService.getUrl(container, blobName, sasToken, true),
+    };
+}
+
+router.post("/", middleware.isLoggedIn, (req, res) => {
+    const content = req.body.context;
+    const id = req.user._id;
+    const filename = content;
+    console.log(id);
+    res.send(generateSasToken("user-image", filename));
+});
+router.post("/manual", (req, res) => {
     const container = "user-image";
     var rawdata = req.body.file;
     // console.log(rawdata);
