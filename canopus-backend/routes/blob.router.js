539f56b5b094e3bc82d237f398310ca0fdd34485
Sushi <tmsusha@gmail.com>
2020-08-20 19:24:24 +0530

    added email route for testing

--- canopus-backend/routes/blob.router.js
+++ canopus-backend/routes/blob.router.js
@@ -6,7 +6,7 @@ const router = require("express").Router(),
     { BlobServiceClient } = require("@azure/storage-blob"),
     azure = require("azure-storage");
 require("dotenv").config();
-AZURE_STORAGE_CONNECTION_STRING =
+const AZURE_STORAGE_CONNECTION_STRING =
     process.env.AZURE_STORAGE_CONNECTION_STRING || "";
 
 var blobService = azure.createBlobService();
@@ -46,38 +46,41 @@ router.post("/",middleware.isLoggedIn,(req,res) =>{
     res.send(generateSasToken('user-image',filename));
 
 })
-router.post("/manual", (req, res) => {
-    const container = "user-image";
-    var rawdata = req.body.file;
-    // console.log(rawdata);
-    var matches = rawdata.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
-    var type = matches[1];
-    var buffer = new Buffer(matches[2], "base64");
-    console.log(buffer);
-    blobService.createBlockBlobFromText(
-        container,
-        req.body.name,
-        buffer,
-        {
-            contentSettings: {
-                contentType: type,
-                //contentEncoding: 'base64'
-            },
-        },
-        function (error, result, response) {
-            if (error) {
-                res.send(error);
-            } else
-                res.send(
-                    JSON.stringify(
-                        "https://canopus.blob.core.windows.net/user-image/" +
-                            req.body.name,
-                    ),
-                );
-            console.log("result", result);
-            console.log("response", response);
-        },
-    );
-});
+
+// This route is for uploading from server without SAS
+
+// router.post("/manual", (req, res) => {
+//     const container = "user-image";
+//     var rawdata = req.body.file;
+//     // console.log(rawdata);
+//     var matches = rawdata.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+//     var type = matches[1];
+//     var buffer = new Buffer(matches[2], "base64");
+//     console.log(buffer);
+//     blobService.createBlockBlobFromText(
+//         container,
+//         req.body.name,
+//         buffer,
+//         {
+//             contentSettings: {
+//                 contentType: type,
+//                 //contentEncoding: 'base64'
+//             },
+//         },
+//         function (error, result, response) {
+//             if (error) {
+//                 res.send(error);
+//             } else
+//                 res.send(
+//                     JSON.stringify(
+//                         "https://canopus.blob.core.windows.net/user-image/" +
+//                             req.body.name,
+//                     ),
+//                 );
+//             console.log("result", result);
+//             console.log("response", response);
+//         },
+//     );
+// });
 
 module.exports = router;
